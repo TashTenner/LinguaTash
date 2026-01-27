c5141561f81508277bb823847d05b3e8422fe4d1
TashTenner <tashfonetikas@gmail.com>
2026-01-27 23:40:37 +0100

    Widen header navigation to prevent clipped nav links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+// line 32: <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
+
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
@@ -29,7 +31,7 @@ const Header = () => {
         </div>
       </Link>
       <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
-        <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
+        <div className="hidden items-center gap-x-6 sm:flex">
           {headerNavLinks
             .filter((link) => link.href !== '/')
             .map((link) => (
