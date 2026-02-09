cdb0d53c7081755eb01ab06f3eed45dc773008ea
TashTenner <tashfonetikas@gmail.com>
2026-02-09 12:09:37 +0100

    Add reading time to blog post header

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -41,12 +41,17 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
       <article>
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 text-center xl:pb-6">
-            <time
-              dateTime={date}
-              className="text-base font-medium text-gray-500 dark:text-gray-400"
-            >
-              {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-            </time>
+            <div className="text-base font-medium text-gray-500 dark:text-gray-400">
+              <time dateTime={date}>
+                {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
+              </time>
+              {content.readingTime && (
+                <>
+                  <span> • </span>
+                  <span>{Math.ceil(content.readingTime.minutes)} minutos de lectura</span>
+                </>
+              )}
+            </div>
             <PageTitle>{title}</PageTitle>
           </header>
 
@@ -81,7 +86,7 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
                 {tags && (
                   <div className="py-6">
                     <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
-                      Tags
+                      Temas
                     </h2>
                     <div className="flex flex-wrap">
                       {tags.map((tag) => (
